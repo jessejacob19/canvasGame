df136802753e1583fc2c8be54a1215c7da7beb7f
jesse collier <jesse.collier97@gmail.com>
2019-06-19 17:09:08 +1200

    made some variables constant

--- control.js
+++ control.js
@@ -1,14 +1,21 @@
 var context, controller, rectangle, loop;
 
+const gravity = 1.5;
+const friction = 0.9;
+const canvasHeight = 180;
+const canvasWidth = 320;
+const rectangleWidth = 32;
+const rectangleHeight = 32
+
 context = document.querySelector("canvas").getContext("2d");
 
-context.canvas.height = 180;
-context.canvas.width = 320;
+context.canvas.height = canvasHeight;
+context.canvas.width = canvasWidth;
 
-rectangle = {
-  height: 32,
+rectangle = { // dimensions and state of rectangle
+  height: rectangleHeight,
   jumping: true,
-  width: 32,
+  width: rectangleWidth,
   x: 144,
   xVelocity: 0,
   y: 0,
@@ -50,28 +57,28 @@ loop = function() {
   if (controller.right) {
     rectangle.xVelocity += 0.5;
   }
-  rectangle.yVelocity += 1.5; //gravity
+  rectangle.yVelocity += gravity;
   rectangle.x += rectangle.xVelocity;
   rectangle.y += rectangle.yVelocity;
-  rectangle.xVelocity *= 0.9; // friction
-  rectangle.yVelocity *= 0.9; // friction
+  rectangle.xVelocity *= friction;
+  rectangle.yVelocity *= friction;
 
   // if rectangle is falling below floor line
-  if (rectangle.y > 180 - 16 - 32) {
+  if (rectangle.y > canvasHeight - 16 - 32) {
     rectangle.jumping = false;
-    rectangle.y = 180 - 16 - 32;
+    rectangle.y = canvasHeight - 16 - 32;
     rectangle.yVelocity = 0;
   }
   //if the rectangle is going off the left of the screen
   if (rectangle.x < -32) {
-    rectangle.x = 320;
-  } else if (rectangle.x > 320) {
+    rectangle.x = canvasWidth;
+  } else if (rectangle.x > canvasWidth) {
     // if rectangle goes past the right boundary
     rectangle.x = -32;
   }
 
   context.fillStyle = "#202020";
-  context.fillRect(0, 0, 320, 180); //x, y, width, height
+  context.fillRect(0, 0, canvasWidth, canvasHeight); //x, y, width, height
   context.fillStyle = "#ff0000"; // red
   context.beginPath();
   context.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
@@ -80,7 +87,7 @@ loop = function() {
   context.lineWidth = 4;
   context.beginPath();
   context.moveTo(0, 164);
-  context.lineTo(320, 164);
+  context.lineTo(canvasWidth, 164);
   context.stroke();
 
   // call update when the browser is ready to draw again
